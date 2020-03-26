75c92893d29e7bc8e31270e5d626d7f31ee57a96
myappdeveloped01 <seichi.nakao.web@gmail.com>
2020-03-26 14:34:31 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    setCount(count => count + 1)
-  }
-  const decrement = () => {
-    setCount(count => count - 1)
-  }
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
   const reset = () => {
-    setCount(count => 0)
-  }
-  const double = () => {
-    setCount(count => count * 2)
-  }
-  const divide3 = () => {
-    setCount(count => {
-      if (count % 3 === 0) {
-        return (count /= 3)
-      } else {
-        return count
-      }
-    })
+    setPrice(price => props.price)
+    setName(price => props.name)
   }
 
   return (
     <div>
-      <div>
-        <div>count: {count}</div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={divide3}>３の倍数の時だけ３で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です
+      </p>
+      <button onClick={() => setPrice(price => price + 1)}>+1</button>
+      <button onClick={() => setPrice(price => price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </div>
   )
+}
+
+// 関数コンポーネントの引数のデフォルトを設定（直接引数に代入して指定する別の方法もある）
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
