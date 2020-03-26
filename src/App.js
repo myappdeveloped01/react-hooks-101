843a8e06a7e0c278199087c3fa086f52df1a11c5
myappdeveloped01 <seichi.nakao.web@gmail.com>
2020-03-26 13:27:44 +0900

    useStateの動作確認

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return <div>This is a template for React app</div>
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count => count + 1)
+  }
+  const decrement = () => {
+    setCount(count => count - 1)
+  }
+  const reset = () => {
+    setCount(count => 0)
+  }
+  const double = () => {
+    setCount(count => count * 2)
+  }
+  const divide3 = () => {
+    setCount(count => {
+      if (count % 3 === 0) {
+        return (count /= 3)
+      } else {
+        return count
+      }
+    })
+  }
+
+  return (
+    <div>
+      <div>
+        <div>count: {count}</div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>３の倍数の時だけ３で割る</button>
+      </div>
+    </div>
+  )
 }
 
 export default App
