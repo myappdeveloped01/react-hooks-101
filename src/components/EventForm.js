498dce6fef6bc434b3882de068340b88d1e2b656
myappdeveloped01 <seichi.nakao.web@gmail.com>
2020-03-28 08:06:32 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
-import AppContext from '../contexts/AppContext'
+import { CREATE_EVENT, DELETE_ALL_EVENTS, ADD_OPERATION_LOG, DELETE_OPERATION_LOGS } from '../actions';
+import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
-    const {state, dispatch} = useContext(AppContext)
+	const { state, dispatch } = useContext(AppContext);
 	// 引数のreducerは、reducers/index.jsにあるexport defaultのeventsコンポーネントになる
 	// const [state, dispatch] = useReducer(reducer, []);
 	const [title, setTitle] = useState('');
@@ -20,6 +21,12 @@ const EventForm = () => {
 			body,
 		});
 
+		dispatch({
+			tipe: ADD_OPERATION_LOG,
+			description: 'イベントを作成しました。',
+			operatedAt: timeCurrentIso8601(),
+		});
+
 		setTitle('');
 		setBody('');
 	};
@@ -30,7 +37,14 @@ const EventForm = () => {
 		// YESとNOが選択できるアラームを表示する
 		// YESを選択すると、trueを返す。（NOはfalse）
 		const result = window.confirm('全てのイベントを本当に削除してもよいですか？');
-		if (result) dispatch({ type: DELETE_ALL_EVENTS });
+		if (result) {
+			dispatch({ type: DELETE_ALL_EVENTS });
+			dispatch({
+				type: ADD_OPERATION_LOG,
+				description: '全てのイベントを削除しました',
+				operatedAt: timeCurrentIso8601(),
+			});
+		}
 	};
 
 	const unCreatable = title === '' || body === '';
