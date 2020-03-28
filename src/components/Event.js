dd2ecbbb8c51ef3635c1577c178199991011c883
myappdeveloped01 <seichi.nakao.web@gmail.com>
2020-03-28 09:34:04 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
-import { DELETE_EVENT } from '../actions';
+import { DELETE_EVENT, ADD_OPERATION_LOG } from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const Event = ({ event }) => {
 	const { dispatch } = useContext(AppContext);
@@ -8,7 +9,14 @@ const Event = ({ event }) => {
 		// YESとNOが選択できるアラームを表示する
 		// YESを選択すると、trueを返す。（NOはfalse）
 		const result = window.confirm(`イベント(id=${event.id})を本当に削除してもよいですか？`);
-		if (result) dispatch({ type: DELETE_EVENT, id: event.id });
+		if (result) {
+			dispatch({ type: DELETE_EVENT, id: event.id });
+			dispatch({
+				type: ADD_OPERATION_LOG,
+				description: `イベント=${event.id})を削除しました。`,
+				operatedAt: timeCurrentIso8601(),
+			});
+		}
 	};
 	return (
 		<tr>
